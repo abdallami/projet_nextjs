b65a0193d3a5bf07dd1982ba185f93a49cbaf57d
abdallami <elikebir381@gmail.com>
2026-06-21 09:09:18 +0100

    fix: prisma client postgresql no adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,23 +1,4 @@
-/*import { PrismaClient } from '@prisma/client'
-//import { PrismaPg } from '@prisma/adapter-pg'  // ✅ import manquant
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-     adapter : new PrismaBetterSqlite3({ url: "file:./dev.db"  })
-  })
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}*/
 import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-import path from 'path'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -25,11 +6,7 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter: new PrismaBetterSqlite3({ 
-      url: `file:${path.join(process.cwd(), 'dev.db')}` // ✅ chemin absolu
-    })
-  })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
