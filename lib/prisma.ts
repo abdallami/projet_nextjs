d41842eaf65d6826f8ce247d27e3554eb4a3390d
abdallami <elikebir381@gmail.com>
2026-06-23 16:15:07 +0100

    modify requete database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { PrismaPg } from "@prisma/adapter-pg"
+/*import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
@@ -11,6 +12,26 @@ export const prisma =
   new PrismaClient({
     adapter: new PrismaPg({
       connectionString: process.env.DATABASE_URL,
+    }),
+  })
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}*/
+
+// lib/prisma.ts
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "@prisma/client"
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
     }),
   })
 
